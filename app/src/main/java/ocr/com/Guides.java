d43b87bced38b1ae99f6adec52569e7e8d7c6f9c
Neo <mingtseng.chang@huafu.com.tw>
2017-05-21 03:41:19 +0800

    change project to Android Studio 2.3.2

---
+++ app/src/main/java/ocr/com/Guides.java
@@ -0,0 +1,62 @@
+package ocr.com;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.Display;
+import android.view.View;
+
+public class Guides extends View {
+    private static String TAG = "Guides";
+    Paint mPaint = new Paint();
+    int sideLength;
+    int deviceWidth;
+    int deviceHeight;
+    String mFace;
+
+    public Guides(Context context, String face) {
+        super(context);
+        mFace = face;
+        mPaint.setColor(Color.RED);
+        //Main == LoadCube
+        Display display = ((MotorScan) context).getWindowManager().getDefaultDisplay();
+        //Display display = ((CameraShow) context).getWindowManager().getDefaultDisplay();
+        deviceWidth = display.getWidth();
+        deviceHeight = display.getHeight();
+        sideLength = (int) (Math.min(deviceWidth, deviceHeight) * .9);
+    }
+
+    public String getSide() {
+        return mFace;
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        int x0, y0, x1, y1, margin;
+        margin = (int) (Math.min(deviceWidth, deviceHeight) * .1);
+        for (int i = 1; i < 3; i++) {
+            x0 = ((deviceWidth - sideLength) / 2) + (sideLength / 2) * i;
+            y0 = margin / 2;
+            y1 = y0 + sideLength;
+            canvas.drawLine(x0, y0, x0, y1, mPaint);
+//			x0 = ((deviceWidth - sideLength) / 2);
+//			x1 = ((deviceWidth - sideLength) / 2) + sideLength;
+//			y0 = margin/2 + (sideLength / 3) * i;
+//			canvas.drawLine(x0,y0,x1,y0,mPaint);
+        }
+
+//		mPaint.setTextSize(60);
+//		canvas.rotate(-90);
+//		x0 = -deviceHeight + margin/2 + 15;
+//		y0 = (deviceWidth - sideLength)/2 + 62;
+//		canvas.drawText("TL", x0,y0, mPaint);
+//		
+//		x0 = -deviceHeight + margin/2 + 15;
+//		y0 = 62;
+//		canvas.drawText(getSide(), x0,y0, mPaint );
+
+        //canvas.drawLine(0, 0, 20, 20, paint);
+        //canvas.drawLine(20, 0, 0, 20, paint);
+    }
+}
---
+++ app/src/main/java/ocr/com/Main.java
@@ -0,0 +1,67 @@
+package ocr.com;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main extends Activity {
+    private Intent intent = new Intent();
+    private Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        intent = this.getIntent();
+        String MotorID = intent.getStringExtra("MotorID");
+        button1 = (Button) findViewById(R.id.bt001);
+
+        Toast.makeText(getApplicationContext(), MotorID, 1000).show();
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent();
+                it.setClass(Main.this, MotorScan.class);
+                startActivity(it);
+            }
+        });
+
+        /**加載字庫,首先判斷字庫檔是否已創建     */
+        File engFile = new File("/mnt/sdcard/OCR-Photo/tessdata/eng.traineddata");
+        if (!engFile.exists()) {
+            //字庫檔未創建，判斷tessdata是否創建
+            String dirPath = "/mnt/sdcard/OCR-Photo/tessdata";
+            File dir = new File(dirPath);
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+            //字庫檔案eng.traineddata，先創造eng.traineddata file並將內容導入
+            File file = new File(dir, "eng.traineddata");
+            try {
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
+                InputStream is = this.getApplicationContext().getResources().openRawResource(R.raw.eng);
+                FileOutputStream fos = new FileOutputStream(file);
+                byte[] buffere = new byte[is.available()];
+                is.read(buffere);
+                fos.write(buffere);
+                is.close();
+                fos.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
